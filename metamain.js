5fb46cb58e0ddad3315d1389c02925103f117d33
CaptainUnknown <captainunknown7@gmail.com>
2022-04-23 06:21:47 +0500

    Console Logs & Boiler plate Commented Out

--- metamain.js
+++ metamain.js
@@ -25,88 +25,88 @@ async function logOut() {
     console.log("logged out");
 }
 
-document.getElementById("btn-login").onclick = login;
-document.getElementById("btn-logout").onclick = logOut;
+async function solveContract() {
+
+    //Get the NFTs of the SC Owned by the User:
+    const contractOne = { chain: "eth", address: user.get('ethAddress'), token_address: "0xA958fd55Be008BE82595939646d37B5Ca4Fe778D" }; //KERGOM OF OLREND MACE CONTRACT
+    const armorNFTsContractOne = await Moralis.Web3API.account.getNFTsForContract(contractOne);
+
+    const contractTwo = { chain: "eth", address: user.get('ethAddress'), token_address: "0x703895bc5c3538a0eFAE736308f76d7bEDf01AfD" }; //GAELINS SONG KOPHER BOW CONTRACT
+    const armorNFTsContractTwo = await Moralis.Web3API.account.getNFTsForContract(contractTwo);
+
+    const contractThree = { chain: "eth", address: user.get('ethAddress'), token_address: "0xea6cca8A0A7A9054e7695f6773476E5FAd43805b" }; //TORDON  OF TRIBEORN CONTRACT
+    const armorNFTsContractThree = await Moralis.Web3API.account.getNFTsForContract(contractThree);
+
+    const contractFour = { chain: "eth", address: user.get('ethAddress'), token_address: "0x73C7F078DBc1D961DF33aDa0EF9caC89c7887801" }; //WENOLIN OF SOMANE SWORD CONTRACT
+    const armorNFTsContractFour = await Moralis.Web3API.account.getNFTsForContract(contractFour);
+    //Returns an Object with an Array of Owned NFTs
 
 
-//Get the NFTs of the SC Owned by the User:
-const contractOne = { chain: "eth", address: user.get('ethAddress'), token_address: "0xA958fd55Be008BE82595939646d37B5Ca4Fe778D" }; //KERGOM OF OLREND MACE CONTRACT
-const armorNFTsContractOne = await Moralis.Web3API.account.getNFTsForContract(contractOne);
+    //Gets & Stores the rarity of the all Owned NFTs
+    var rarityOwnedContractOne = [];
+    var rarityOwnedContractTwo = [];
+    var rarityOwnedContractThree = [];
+    var rarityOwnedContractFour = [];
+    //String Arrays rarities of all Contracts
 
-const contractTwo = { chain: "eth", address: user.get('ethAddress'), token_address: "0x703895bc5c3538a0eFAE736308f76d7bEDf01AfD" }; //GAELINS SONG KOPHER BOW CONTRACT
-const armorNFTsContractTwo = await Moralis.Web3API.account.getNFTsForContract(contractTwo);
-
-const contractThree = { chain: "eth", address: user.get('ethAddress'), token_address: "0xea6cca8A0A7A9054e7695f6773476E5FAd43805b" }; //TORDON  OF TRIBEORN CONTRACT
-const armorNFTsContractThree = await Moralis.Web3API.account.getNFTsForContract(contractThree);
-
-const contractFour = { chain: "eth", address: user.get('ethAddress'), token_address: "0x73C7F078DBc1D961DF33aDa0EF9caC89c7887801" }; //WENOLIN OF SOMANE SWORD CONTRACT
-const armorNFTsContractFour = await Moralis.Web3API.account.getNFTsForContract(contractFour);
-//Returns an Object with an Array of Owned NFTs
-
-
-//Gets & Stores the rarity of the all Owned NFTs
-var rarityOwnedContractOne = [];
-var rarityOwnedContractTwo = [];
-var rarityOwnedContractThree = [];
-var rarityOwnedContractFour = [];
-//String Arrays rarities of all Contracts
-
-//Grabing Values from returned objects & storing them in rarity arrays
-for (var i = 0; i < armorNFTsContractOne.result.length; i++) {
-    rarityOwnedContractOne[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
-}
-
-for (var i = 0; i < armorNFTsContractTwo.result.length; i++) {
-    rarityOwnedContractTwo[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
-}
-
-for (var i = 0; i < armorNFTsContractThree.result.length; i++) {
-    rarityOwnedContractThree[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
-}
-
-for (var i = 0; i < armorNFTsContractFour.result.length; i++) {
-    rarityOwnedContractFour[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
-}
-
-var standardOne = false;
-var specialOne = false;
-var rareOne = false;
-var legendaryOne = false;
-
-var standardTwo = false;
-var specialTwo = false;
-var rareTwo = false;
-var legendaryTwo = false;
-
-var standardThree = false;
-var specialThree = false;
-var rareThree = false;
-var legendaryThree = false;
-
-var standardFour = false;
-var specialFour = false;
-var rareFour = false;
-var legendaryFour = false;
-
-//Giving the User Download Links for their respective rarities (CONFIRM FIRST)
-for (var i = 0; i < rarityOwnedContractOne.length; i++) {
-    if (rarityOwnedContractOne[i] == "Standard") {
-        standardOne = true;
-        // https://drive.google.com/drive/folders/1_Jvj9c-tFsXgDbYu9Ne-35buE1-VPb63?usp=sharing
+    //Grabing Values from returned objects & storing them in rarity arrays
+    for (var i = 0; i < armorNFTsContractOne.result.length; i++) {
+        rarityOwnedContractOne[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
     }
-    if (rarityOwnedContractOne[i] == "Special") {
-        specialOne = true;
+
+    for (var i = 0; i < armorNFTsContractTwo.result.length; i++) {
+        rarityOwnedContractTwo[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
+    }
+
+    for (var i = 0; i < armorNFTsContractThree.result.length; i++) {
+        rarityOwnedContractThree[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
+    }
+
+    for (var i = 0; i < armorNFTsContractFour.result.length; i++) {
+        rarityOwnedContractFour[i] = armorNFTsContractOne.result[i].metadata.attributes[2].value;
+    }
+
+    var standardOne = false;
+    var specialOne = false;
+    var rareOne = false;
+    var legendaryOne = false;
+
+    var standardTwo = false;
+    var specialTwo = false;
+    var rareTwo = false;
+    var legendaryTwo = false;
+
+    var standardThree = false;
+    var specialThree = false;
+    var rareThree = false;
+    var legendaryThree = false;
+
+    var standardFour = false;
+    var specialFour = false;
+    var rareFour = false;
+    var legendaryFour = false;
+
+    //Giving the User Download Links for their respective rarities (CONFIRM FIRST)
+    for (var i = 0; i < rarityOwnedContractOne.length; i++) {
+        if (rarityOwnedContractOne[i] == "Standard") {
+            standardOne = true;
+            // https://drive.google.com/drive/folders/1_Jvj9c-tFsXgDbYu9Ne-35buE1-VPb63?usp=sharing
+        }
+        //if (rarityOwnedContractOne[i] == "Special") {
+        //specialOne = true;
         // https://drive.google.com/drive/folders/1vXOyL445IKlypbyi7l9GqtSZleZM2DGJ?usp=sharing
     }
-    if (rarityOwnedContractOne[i] == "Rare") {
-        rareOne = true;
-        // https://drive.google.com/drive/folders/1YpCoqDZcT-rYfUM5boVafPaqUUaHXfWF?usp=sharing
-    }
-    if (rarityOwnedContractOne[i] == "Legendary") {
-        legendaryOne = true;
-        // https://drive.google.com/drive/folders/1UhC1H0VrYR_d3zdppXUnU1ZXM6YPDhVv?usp=sharing
-    }
+    //if (rarityOwnedContractOne[i] == "Rare") {
+    //  rareOne = true;
+    // https://drive.google.com/drive/folders/1YpCoqDZcT-rYfUM5boVafPaqUUaHXfWF?usp=sharing
 }
+//if (rarityOwnedContractOne[i] == "Legendary") {
+//  legendaryOne = true;
+// https://drive.google.com/drive/folders/1UhC1H0VrYR_d3zdppXUnU1ZXM6YPDhVv?usp=sharing
+//}
+//}
+
+/* 
 
 for (var i = 0; i < rarityOwnedContractTwo.length; i++) {
     if (rarityOwnedContractTwo[i] == "Standard") {
@@ -163,9 +163,19 @@ for (var i = 0; i < rarityOwnedContractFour.length; i++) {
         legendaryFour = true;
         // https://drive.google.com/drive/folders/1h2pzBL7jvEJ6Nyx-UegiD3ON0YhaZZYr?usp=sharing
     }
-}
+} */
+//}
 
-$(true)(function() {
-    var r = $('<input type="button" value="download button"/>');
-    $("body").append("<button><a href='" + "https://example.com/" + "'>Download Source</a></button>");
-});
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
+document.getElementById("btn-download").onclick = solveContract;
+console.log(standardOne = true);
+console.log(armorNFTsContractOne);
+console.log(rarityOwnedContractOne);
+
+
+
+//$(true)(function() {
+//  var r = $('<input type="button" value="download button"/>');
+//$("body").append("<button><a href='" + "https://example.com/" + "'>Download Source</a></button>");
+//});
