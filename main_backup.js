6f75626af01c26583e4a6486e1dc891da600ac3c
CaptainUnknown <captainunknown7@gmail.com>
2022-04-25 08:57:37 +0500

    Finalized

--- main_backup.js
+++ main_backup.js
@@ -48,14 +48,6 @@ document.getElementById("btn-login").addEventListener('click', () => {
 }, false);
 
 
-//============REMOVE THIS IN FINAL VERSION======================
-document.getElementById("notestnft").style.visibility = 'hidden';
-document.getElementById("test1").style.visibility = 'hidden';
-document.getElementById("test2").style.visibility = 'hidden';
-document.getElementById("test3").style.visibility = 'hidden';
-document.getElementById("test4").style.visibility = 'hidden';
-//==============================================================
-
 //==================INIT ELEMENTS===============================
 document.getElementById("meta1").style.visibility = 'hidden';
 document.getElementById("meta2").style.visibility = 'hidden';
@@ -73,6 +65,23 @@ document.getElementById("meta13").style.visibility = 'hidden';
 document.getElementById("meta14").style.visibility = 'hidden';
 document.getElementById("meta15").style.visibility = 'hidden';
 document.getElementById("meta16").style.visibility = 'hidden';
+
+document.getElementById("item1").style.visibility = 'hidden';
+document.getElementById("item2").style.visibility = 'hidden';
+document.getElementById("item3").style.visibility = 'hidden';
+document.getElementById("item4").style.visibility = 'hidden';
+document.getElementById("item5").style.visibility = 'hidden';
+document.getElementById("item6").style.visibility = 'hidden';
+document.getElementById("item7").style.visibility = 'hidden';
+document.getElementById("item8").style.visibility = 'hidden';
+document.getElementById("item9").style.visibility = 'hidden';
+document.getElementById("item10").style.visibility = 'hidden';
+document.getElementById("item11").style.visibility = 'hidden';
+document.getElementById("item12").style.visibility = 'hidden';
+document.getElementById("item13").style.visibility = 'hidden';
+document.getElementById("item14").style.visibility = 'hidden';
+document.getElementById("item15").style.visibility = 'hidden';
+document.getElementById("item16").style.visibility = 'hidden';
 
 document.getElementById("nonfts").style.visibility = 'hidden';
 
@@ -198,20 +207,20 @@ async function getNFTObjs() {
         for (var i = 0; i < armorNFTsContractOneVar.result.length; i++) {
             //var rarityOwnedContractOneMetaData = JSON.parse(armorNFTsContractOneVar.result[i].metadata);
             var oneTempMeta = JSON.parse(armorNFTsContractOneVar.result[i].metadata);
-            rarityOwnedContractOne[i] = oneTempMeta.attributes[0].value; //change 0 to 2
+            rarityOwnedContractOne[i] = oneTempMeta.attributes[2].value; //change 0 to 2
         }
     }
     if (armorNFTsContractTwoVar.result.length != 0) {
         for (var i = 0; i < armorNFTsContractTwoVar.result.length; i++) {
             //var rarityOwnedContractTwoMetaData = JSON.parse(armorNFTsContractTwoVar.result[i].metadata);
             var twoTempMeta = JSON.parse(armorNFTsContractTwoVar.result[i].metadata);
-            rarityOwnedContractTwo[i] = twoTempMeta.attributes[0].value;
+            rarityOwnedContractTwo[i] = twoTempMeta.attributes[2].value;
         }
     }
     if (armorNFTsContractThreeVar.result.length != 0) {
         for (var i = 0; i < armorNFTsContractThreeVar.result.length; i++) {
             var threeTempMeta = JSON.parse(armorNFTsContractThreeVar.result[i].metadata);
-            rarityOwnedContractThree[i] = threeTempMeta.attributes[0].value;
+            rarityOwnedContractThree[i] = threeTempMeta.attributes[2].value;
             console.log(rarityOwnedContractThree[i]);
         }
     }
@@ -219,101 +228,31 @@ async function getNFTObjs() {
         for (var i = 0; i < armorNFTsContractFourVar.result.length; i++) {
             //var rarityOwnedContractFourMetaData = JSON.parse(armorNFTsContractFourVar.result[i].metadata);
             var fourTempMeta = JSON.parse(armorNFTsContractFourVar.result[i].metadata);
-            rarityOwnedContractFour[i] = fourTempMeta.attributes[0].value;
+            rarityOwnedContractFour[i] = fourTempMeta.attributes[2].value;
         }
     }
 
-
-
-
-
-    //========================================= REMOVE THIS IN FINAL VERSION =========================================================
-    var MysteryOne = false;
-    var MysteryTwo = false;
-    var MysteryThree = false;
-    var MysteryFour = false;
-    for (var i = 0; i <= rarityOwnedContractOne.length; i++) {
-        if (rarityOwnedContractOne[i] == "Mystery") {
-            MysteryOne = true;
-            // https://drive.google.com/drive/folders/1_Jvj9c-tFsXgDbYu9Ne-35buE1-VPb63?usp=sharing
-        }
-    }
-    for (var i = 0; i <= rarityOwnedContractTwo.length; i++) {
-        if (rarityOwnedContractTwo[i] == "Mystery") {
-            MysteryTwo = true;
-            // https://drive.google.com/drive/folders/1_Jvj9c-tFsXgDbYu9Ne-35buE1-VPb63?usp=sharing
-        }
-    }
-    for (var i = 0; i <= rarityOwnedContractThree.length; i++) {
-        if (rarityOwnedContractThree[i] == "Mystery") {
-            MysteryThree = true;
-            // https://drive.google.com/drive/folders/1_Jvj9c-tFsXgDbYu9Ne-35buE1-VPb63?usp=sharing
-        }
-    }
-    for (var i = 0; i <= rarityOwnedContractFour.length; i++) {
-        if (rarityOwnedContractFour[i] == "Mystery") {
-            MysteryFour = true;
-            // https://drive.google.com/drive/folders/1_Jvj9c-tFsXgDbYu9Ne-35buE1-VPb63?usp=sharing
-        }
-    }
-
-    //log only
-    console.log(rarityOwnedContractOne);
-    console.log(rarityOwnedContractOne.length);
-    console.log(rarityOwnedContractTwo);
-    console.log(rarityOwnedContractTwo.length);
-    console.log(rarityOwnedContractThree);
-    console.log(rarityOwnedContractThree.length);
-    console.log(rarityOwnedContractFour);
-    console.log(rarityOwnedContractFour.length);
-
-
-    if (MysteryOne) {
-        document.getElementById("test1").style.visibility = 'visible';
-    }
-    if (MysteryTwo) {
-        document.getElementById("test2").style.visibility = 'visible';
-    }
-    if (MysteryThree) {
-        document.getElementById("test3").style.visibility = 'visible';
-    }
-    if (MysteryFour) {
-        document.getElementById("test4").style.visibility = 'visible';
-    }
-    if (MysteryOne == false && MysteryTwo == false && MysteryThree == false && MysteryFour == false) {
-        document.getElementById("notestnft").style.visibility = 'visible';
-    }
-    console.log(MysteryOne);
-    console.log(MysteryTwo);
-    console.log(MysteryThree);
-    console.log(MysteryFour);
-    //=================================================================================================================================
-
-
-
-
-    /*
     var standardOne = false;
     var specialOne = false;
     var rareOne = false;
     var legendaryOne = false;
-    
+
     var standardTwo = false;
     var specialTwo = false;
     var rareTwo = false;
     var legendaryTwo = false;
-    
+
     var standardThree = false;
     var specialThree = false;
     var rareThree = false;
     var legendaryThree = false;
-    
+
     var standardFour = false;
     var specialFour = false;
     var rareFour = false;
     var legendaryFour = false;
-    
-    
+
+
     //Giving the User Download Links for their respective rarities
     for (var i = 0; i < rarityOwnedContractOne.length; i++) {
         if (rarityOwnedContractOne[i] == "Standard") {
@@ -333,8 +272,8 @@ async function getNFTObjs() {
             //https://drive.google.com/drive/folders/1UhC1H0VrYR_d3zdppXUnU1ZXM6YPDhVv?usp=sharing
         }
     }
-    
-    
+
+
     for (var i = 0; i < rarityOwnedContractTwo.length; i++) {
         if (rarityOwnedContractTwo[i] == "Standard") {
             standardTwo = true;
@@ -353,7 +292,7 @@ async function getNFTObjs() {
             // https://drive.google.com/drive/folders/1vVgsJjUO55Gs1AGGTrRVFa7gS3ZdN-Wv?usp=sharing
         }
     }
-    
+
     for (var i = 0; i < rarityOwnedContractThree.length; i++) {
         if (rarityOwnedContractThree[i] == "Standard") {
             standardThree = true;
@@ -372,7 +311,7 @@ async function getNFTObjs() {
             // https://drive.google.com/drive/folders/1jvQw-e5HO_VlcvpttSHlHQJMU5GtN7Ss?usp=sharing
         }
     }
-    
+
     for (var i = 0; i < rarityOwnedContractFour.length; i++) {
         if (rarityOwnedContractFour[i] == "Standard") {
             standardFour = true;
@@ -391,175 +330,175 @@ async function getNFTObjs() {
             // https://drive.google.com/drive/folders/1h2pzBL7jvEJ6Nyx-UegiD3ON0YhaZZYr?usp=sharing
         }
     }
-    
-    
-    
+
+
+
     if (standardOne) {
         document.getElementById("meta1").style.visibility = 'visible';
     }
     if (specialOne) {
-        document.getElementById("meta2").style.display = "visible";
+        document.getElementById("meta2").style.visibility = "visible";
     }
     if (rareOne) {
-        document.getElementById("meta3").style.display = "visible";
+        document.getElementById("meta3").style.visibility = "visible";
     }
     if (legendaryOne) {
-        document.getElementById("meta4").style.display = "visible";
+        document.getElementById("meta4").style.visibility = "visible";
     }
-    
-    
+
+
     if (standardTwo) {
-        document.getElementById("meta5").style.display = "visible";
+        document.getElementById("meta5").style.visibility = "visible";
     }
     if (specialTwo) {
-        document.getElementById("meta6").style.display = "visible";
+        document.getElementById("meta6").style.visibility = "visible";
     }
     if (rareTwo) {
-        document.getElementById("meta7").style.display = "visible";
+        document.getElementById("meta7").style.visibility = "visible";
     }
     if (legendaryTwo) {
-        document.getElementById("meta8").style.display = "visible";
+        document.getElementById("meta8").style.visibility = "visible";
     }
-    
-    
+
+
     if (standardThree) {
-        document.getElementById("meta9").style.display = "visible";
+        document.getElementById("meta9").style.visibility = "visible";
     }
     if (specialThree) {
-        document.getElementById("meta10").style.display = "visible";
+        document.getElementById("meta10").style.visibility = "visible";
     }
     if (rareThree) {
-        document.getElementById("meta11").style.display = "visible";
+        document.getElementById("meta11").style.visibility = "visible";
     }
     if (legendaryThree) {
-        document.getElementById("meta12").style.display = "visible";
+        document.getElementById("meta12").style.visibility = "visible";
     }
-    
-    
+
+
     if (standardFour) {
-        document.getElementById("meta13").style.display = "visible";
+        document.getElementById("meta13").style.visibility = "visible";
     }
     if (specialFour) {
-        document.getElementById("meta14").style.display = "visible";
+        document.getElementById("meta14").style.visibility = "visible";
     }
     if (rareFour) {
-        document.getElementById("meta15").style.display = "visible";
+        document.getElementById("meta15").style.visibility = "visible";
     }
     if (legendaryFour) {
-        document.getElementById("meta16").style.display = "visible";
+        document.getElementById("meta16").style.visibility = "visible";
     }
-    
-    
+
+
     if (standardOne == false) {
         let tempElement = document.getElementById("meta1");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item1");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (specialOne == false) {
         let tempElement = document.getElementById("meta2");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item2");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (rareOne == false) {
         let tempElement = document.getElementById("meta3");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item3");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (legendaryOne == false) {
         let tempElement = document.getElementById("meta4");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item4");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
-    
-    
+
+
+
     if (standardTwo == false) {
         let tempElement = document.getElementById("meta5");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item5");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (specialTwo == false) {
         let tempElement = document.getElementById("meta6");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item6");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (rareTwo == false) {
         let tempElement = document.getElementById("meta7");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item7");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (legendaryTwo == false) {
         let tempElement = document.getElementById("meta8");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item8");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
-    
-    
+
+
+
     if (standardThree == false) {
         let tempElement = document.getElementById("meta9");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item9");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (specialThree == false) {
         let tempElement = document.getElementById("meta10");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item10");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (rareThree == false) {
         let tempElement = document.getElementById("meta11");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item11");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (legendaryThree == false) {
         let tempElement = document.getElementById("meta12");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item12");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
-    
+
+
     if (standardFour == false) {
         let tempElement = document.getElementById("meta13");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item13");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (specialFour == false) {
         let tempElement = document.getElementById("meta14");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item14");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (rareFour == false) {
         let tempElement = document.getElementById("meta15");
         tempElement.parentNode.removeChild(tempElement);
         let tempSec = document.getElementById("item15");
         tempSec.parentNode.removeChild(tempSec);
     }
-    
+
     if (legendaryFour == false) {
         let tempElement = document.getElementById("meta16");
         tempElement.parentNode.removeChild(tempElement);
@@ -569,21 +508,20 @@ async function getNFTObjs() {
 
 
 
-    if (standardOne || specialOne || rareOne || legendaryOne
-    || standardTwo || specialTwo || rareTwo || legendaryTwo
-    || standardThree || specialThree || rareThree || legendaryThree
-    || standardFour || specialFour || rareFour || legendaryFour) {
+    if (standardOne || specialOne || rareOne || legendaryOne ||
+        standardTwo || specialTwo || rareTwo || legendaryTwo ||
+        standardThree || specialThree || rareThree || legendaryThree ||
+        standardFour || specialFour || rareFour || legendaryFour) {
         let tempElement = document.getElementById("nonfts");
         tempElement.parentNode.removeChild(tempElement);
     }
-    
-    if (standardOne == false && specialOne == false && rareOne == false && legendaryOne == false
-    && standardTwo == false && specialTwo == false && rareTwo == false && legendaryTwo == false
-    && standardThree == false && specialThree == false && rareThree == false && legendaryThree == false
-    && standardFour == false && specialFour == false && rareFour == false && legendaryFour == false){
+
+    if (standardOne == false && specialOne == false && rareOne == false && legendaryOne == false &&
+        standardTwo == false && specialTwo == false && rareTwo == false && legendaryTwo == false &&
+        standardThree == false && specialThree == false && rareThree == false && legendaryThree == false &&
+        standardFour == false && specialFour == false && rareFour == false && legendaryFour == false) {
         document.getElementById("nonfts").style.visibility = 'visible';
     }
-    */
 
 };
 
